e7ef09ceb8a25f7a41c5dfc6e592b02b0f197260
Hrithik <hc388@njit.edu>
2020-06-30 02:02:30 -0400

    Added reach router

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Link } from "@reach/router";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const App = () => {
+  return (
+    <React.StrictMode>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
